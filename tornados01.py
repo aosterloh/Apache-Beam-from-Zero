fe9c7b7d086922798dbf1eeb2d848e0a1bb0a628
aosterloh <aosterloh@google.com>
2018-12-03 16:10:38 +0100

    v2

--- tornados01.py
+++ tornados01.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
    with beam.Pipeline('DirectRunner') as pipeline:
 
       airports = (pipeline
-         | beam.io.ReadFromText('test_small10.csv')
+         | beam.io.ReadFromText('test_small.csv')
          | beam.Map(lambda line: next(csv.reader([line])))
          | beam.Map(lambda fields: (fields[3], (fields[30])))
       	 | beam.io.textio.WriteToText('extracted_tornados')   
