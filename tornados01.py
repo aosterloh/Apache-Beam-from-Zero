0a560a9f5e4b787a4e4533b95958d8d82208e347
aosterloh <aosterloh@google.com>
2018-12-03 16:48:50 +0100

    Add files via upload

--- tornados01.py
+++ tornados01.py
@@ -1,13 +1,9 @@
 import apache_beam as beam
-import csv
 
-if __name__ == '__main__':
-   with beam.Pipeline('DirectRunner') as pipeline:
+pipeline =  beam.Pipeline('DirectRunner')
 
-      airports = (pipeline
-         | beam.io.ReadFromText('test_small.csv')
-         | beam.Map(lambda line: next(csv.reader([line])))
-         | beam.Map(lambda fields: (fields[3], (fields[30])))
-      	 | beam.io.textio.WriteToText('extracted_tornados')   
-      )
-      pipeline.run()
+airports = (pipeline
+ | beam.io.ReadFromText('test_small.csv')
+ | beam.io.textio.WriteToText('extracted_tornados')   
+)
+pipeline.run()
