a2d11dbf099a5139e79433329ec7ce433ad64292
aosterloh <aosterloh@google.com>
2018-12-03 15:57:24 +0100

    v1

---
+++ tornados01.py
@@ -0,0 +1,13 @@
+import apache_beam as beam
+import csv
+
+if __name__ == '__main__':
+   with beam.Pipeline('DirectRunner') as pipeline:
+
+      airports = (pipeline
+         | beam.io.ReadFromText('test_small10.csv')
+         | beam.Map(lambda line: next(csv.reader([line])))
+         | beam.Map(lambda fields: (fields[3], (fields[30])))
+      	 | beam.io.textio.WriteToText('extracted_tornados')   
+      )
+      pipeline.run()
