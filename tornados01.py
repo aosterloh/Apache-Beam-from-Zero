97297c771afed250fc2d2d00f3b16e1d3ce41085
aosterloh <aosterloh@google.com>
2018-12-04 12:47:09 +0100

    Delete tornados01.py

--- tornados01.py
+++
@@ -1,9 +0,0 @@
-import apache_beam as beam
-
-pipeline =  beam.Pipeline('DirectRunner')
-
-airports = (pipeline
- | beam.io.ReadFromText('test_small.csv')
- | beam.io.textio.WriteToText('extracted_tornados')   
-)
-pipeline.run()
