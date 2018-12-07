e77775b3b4005665c8280ba374e0f4aebe14c767
aosterloh <aosterloh@google.com>
2018-12-07 14:01:05 +0100

    Add files via upload

--- run04.sh
+++ run04.sh
@@ -1,9 +1,8 @@
 PROJECT=data-academy-2018
 BUCKET=alex-testdata
-python -m tornadoes03 --input gs://$BUCKET/weather/gsod_medium.csv \
+python -m tornadoes04 --input gs://$BUCKET/weather/gsod_medium.csv \
   --output gs://$BUCKET/output/ \
   --runner DataflowRunner \
   --project $PROJECT \
   --region europe-west1 \
-  --temp_location gs://$BUCKET/temp_location \
-  --setup_file ./setup.py
+  --temp_location gs://$BUCKET/temp_location 
